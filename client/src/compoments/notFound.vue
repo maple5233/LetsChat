47cb3b61d86144a477da2e69a91c9eb375ac83be
maple5233 <326153996@qq.com>
2017-04-09 17:34:07 +0800

    add(notFound): 404组件

---
+++ client/src/compoments/notFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="main not-found">
+    <h1>404</h1>
+    <h2>阿叻，你好像来到了异次元</h2>
+    <br/>
+    <h2>点<router-link to="/">这里</router-link>回到起点</h2>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  .not-found {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
