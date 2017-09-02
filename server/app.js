323a1d56841046f93d2f746221988b0269e6334e
maple5233 <326153996@qq.com>
2017-09-02 19:24:39 +0800

    init 调通后端

--- server/app.js
+++ server/app.js
@@ -34,7 +34,10 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500).end();
+    res.status(err.status || 500).json({
+        err: err.toString(),
+        status: err.status || 500
+    }).end();
 });
 
 module.exports = app;
