31a1f3fbff1d7d98b8e3773d4414225fabb410b1
maple5233 <326153996@qq.com>
2017-09-03 19:41:46 +0800

    chore 配置数据库

--- server/config.js
+++ server/config.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const connectionString = 'mongodb://localhost/LetsChat';
 
-module.exports = mongoose.createConnection('mongodb://localhost/LetsChat');
+const options = {
+    server: {
+        auto_reconnect: true,
+        poolSize: 10
+    }
+};
+
+
+module.exports = mongoose.createConnection(connectionString, options);
 
 module.exports.db = mongoose.connection;
 module.exports.Schema = mongoose.Schema;
