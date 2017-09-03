6e457ed3b0a64a364f6c0f905ca10843f4e214f1
maple5233 <326153996@qq.com>
2017-09-03 19:51:51 +0800

    feat 消息模型

---
+++ models/message.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const mongoose = require('../config');
+const Schema = mongoose.Schema;
+
+let MessageSchema = new Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    content: {
+        type: String,
+        required: true
+    }
+});
+
+MessageSchema.statics = {
+    fetchById: (id, cb) => {
+        return Message
+            .findById(id)
+            .exec(cb);
+    },
+    fetch: (json, cb) => {
+        return Message
+            .find(json)
+            .sort({
+                '_id': -1
+            })
+            .exec(cb);
+    },
+    fetchOne: function(id, cb) {
+        return Message
+            .findById(id)
+            .populate('author')
+            .exec(cb);
+    }
+};
+
+
+const Message = mongoose.model('Message', MessageSchema);
+module.exports = Message;
