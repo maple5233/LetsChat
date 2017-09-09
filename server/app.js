4eafe3de6b2d4117e9919393ad1c73e9d2f1e4c6
maple5233 <326153996@qq.com>
2017-09-09 17:23:06 +0800

    feat 引入socket.io

--- server/app.js
+++ server/app.js
@@ -7,10 +7,17 @@ const bodyParser = require('body-parser');
 const debug = require('debug')('LetsChat');
 const mongoose = require('mongoose');
 const app = express();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io').listen(httpServer);
+
+const Message = require('./models/message.js');
+const Room = require('./models/Room.js');
+const User = require('./models/User.js');
 
 const config = require('./config');
 const index = require('./routes/index');
 const users = require('./routes/users');
+
 
 // 连接数据库
 config.on('error', () => {
@@ -33,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+const onlineUsers = {}; //存储在线用户列表的对象
 
 app.use((req, res, next) => {
     const err = new Error('Not Found');
@@ -48,5 +56,42 @@ app.use((err, req, res, next) => {
         status: err.status || 500
     }).end();
 });
+
+
+io.sockets.on('connection', (socket) => {
+
+    //有人上线
+    socket.on('login', (data) => {
+        // 将上线的用户名存储为 socket 对象的属性，以区分每个 socket 对象，方便后面使用
+        socket.name = data.user;
+        socket.roomId = data.roomId;
+        // onlineUsers 对象中不存在该用户名则插入该用户名
+        if (!onlineUsers[data.user]) {
+            onlineUsers[data.user] = data.user;
+        }
+    });
+
+    // 有人说话
+    socket.on('say', (data) => {
+        const roomId = data.roomId;
+        const clients = io.sockets.clients();
+        // 遍历找到该聊天室的用户
+        clients.forEach(function (client) {
+            if (client.roomId === roomId) {
+                client.emit('say', data.message);
+            }
+        });
+    });
+
+
+    //有人下线
+    socket.on('disconnect', function() {
+        // 若 onlineUsers 对象中保存了该用户名
+        if (onlineUsers[socket.name]) {
+            delete onlineUsers[socket.name];
+        }
+    });
+});
+
 
 module.exports = app;
